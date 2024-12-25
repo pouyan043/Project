ac37d7a08f8e01de611627c05fdf2d4da4eda0cf
pouyan043 <121458130+pouyan043@users.noreply.github.com>
2024-12-25 03:25:04 -0800

    Update producer.go

--- producer/producer.go
+++ producer/producer.go
@@ -9,31 +9,31 @@ import (
 )
 
 func main() {
-	//  اتصال به Redis
+	//  connecting to  Redis
 	client := asynq.NewClient(asynq.RedisClientOpt{Addr: "localhost:6379"})
 	defer client.Close() //بستن redis
 
-	//  تعریف payload
+	//   payload explain 
 	payload := map[string]interface{}{
 		"user_id":   42,
 		"task_name": "send_email",
 	}
 
-	//  قرار دادن payload تو ارایه json
+	//  setting payload in json array
 	payloadBytes, err := json.Marshal(payload)
 	if err != nil {
 		log.Fatalf("invalid to send paylaoad into json%v ", err)
 	}
 
-	//  ایجاد شغل و فرساتادن دیتا
+	//  make a job and sending in database
 	task := asynq.NewTask("email:send", payloadBytes)
 
-	//  فرستادن شغل به صف
+	//  seding job oin queue
 	info, err := client.Enqueue(task, asynq.Queue("default"), asynq.MaxRetry(5), asynq.Timeout(30*time.Second))
 	if err != nil {
 		log.Fatalf("invalid to send job %v", err)
 	}
 
-	//  نشون دادن اطلاعات شغل
+	//  show jobs informations
 	log.Printf("job sent : ID=%s", info.ID)
 }
